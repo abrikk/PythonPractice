2367f44ee9ae7653fcfd6fe7dbcaca990c1fe6ae
abrikk <abrormaksudov2207@gmail.com>
2022-04-08 18:10:53 +0500

    add programm which prints all divisors of natural number

---
+++ divisors_of_number.py
@@ -0,0 +1,8 @@
+""" Programm which prints all divisors of natural numbers """
+num = int(input("Enter a natural number: "))
+all_divisors = []
+for i in range(1, num + 1):
+    if num % i == 0:
+        all_divisors.append(str(i))
+
+print(f"The all divisors of {num}: {', '.join(all_divisors)}")
